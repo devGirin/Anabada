c59f6203953dfb43d234ec832164274b2feefe5a
GiwoongBang <bgw6713@naver.com>
2023-06-15 23:00:46 +0900

    Initial commit

---
+++ src/test/java/com/anabada/backend/AnabadaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anabada.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnabadaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
